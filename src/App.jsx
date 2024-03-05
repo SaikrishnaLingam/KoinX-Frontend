34c493dcc3e2a988e45ccc7da89c02683cea691c
Saikrishna <lingams2001@gmail.com>
2024-03-06 01:58:43 +0530

    setup router, created header and navbar and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
-
+import Header from './components/Header'
+import { Outlet } from "react-router-dom";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
+            <Header />
+            <Outlet />
             <Footer />
         </>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import TrendingCoins from './TrendingCoins'
 
 const Footer = () => {
     return (
-        <>
+        <div>
             <h1 className='my-5 text-left text-xl font-bold'>You May Also Like</h1>
             <TrendingCoins />
             <h1 className='my-5 text-left text-xl font-bold'>Trending Coins</h1>
             <TrendingCoins />
-        </>
+        </div>
     )
 }
 
