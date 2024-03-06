7f3b84918df343733fe35549c3cad613f82817a9
Saikrishna <lingams2001@gmail.com>
2024-03-07 02:23:43 +0530

    fixed layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ import Tokenomics from './components/Tokenomics';
 import Team from './components/Team';
 import Sentiment from './components/Sentiment';
 import AboutBitcoin from './components/AboutBitcoin';
+import Overview from './components/Overview';
 
 function App() {
 
     return (
         <>
             <Header />
-            <div className='bg-slate-200'>
+            <div className='bg-slate-200 '>
                 {/* <Outlet /> */}
-                
-                <Sentiment />
-                <AboutBitcoin />
-                <Tokenomics />
-                <Team />
+                <div className='flex flex-row'>
+                    <div className='w-3/4'>
+                        <Overview />
+                        <Sentiment />
+                        <AboutBitcoin />
+                        <Tokenomics />
+                        <Team />
+                    </div>
+                    <div className='w-1/4'>
+                        side stuff
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
