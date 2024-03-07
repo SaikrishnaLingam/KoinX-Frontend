56e93ec9bbb358e50c51de366dafc17dcdfc76a6
Saikrishna <lingams2001@gmail.com>
2024-03-07 16:05:56 +0530

    made minor css changes

--- src/components/BitcoinGraph.jsx
+++ src/components/BitcoinGraph.jsx
@@ -3,8 +3,8 @@ import TradingViewWidget from './TradingViewWidget';
 
 const BitcoinGraph = () => {
   return (
-    <div>
-        <div className='rounded-lg overflow-y-hidden'>
+    <div className='bg-slate-200'>
+        <div className='rounded-xl overflow-y-hidden p-12'>
             <TradingViewWidget />
         </div>
     </div>
